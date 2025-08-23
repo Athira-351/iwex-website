516ab046c4b62ffe545f508c1c94c6032bdaf62b
Athira Anil <athiraanil351@gmail.com>
2025-08-23 17:36:16 +0530

    corrected text

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -198,7 +198,7 @@ const Products = () => {
                   <h3 className="text-xl font-semibold mb-4">
                     {module.title}
                   </h3>
-                  <p className="text-gray-700">{module.desc}</p>
+                  <p className="text-gray-700" dangerouslySetInnerHTML={{ __html: module.desc }}></p>
                 </motion.div>
               </div>
             ))}
